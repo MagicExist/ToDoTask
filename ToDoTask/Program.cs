5a8a7052e3d8ef38f1f83abfd09dc47503f70c75
Johhan <Johhanparra1@gmail.com>
2023-09-16 20:02:28 -0500

    Added menu function creator

--- ToDoTask/Program.cs
+++ ToDoTask/Program.cs
@@ -6,11 +6,17 @@ namespace ToDoTask
     {
         enum iconList { AT, CAT, SAD, HAPPY }
 
+        static void Menu(List<string> options) 
+        {
+            for (int i = 0; i < options.Count; i++) 
+            {
+                Console.WriteLine($"{i + 1}.{options[i]}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Pages page = new Pages("Exercises",iconList.HAPPY.ToString());
 
-            Console.WriteLine(page.Icon);
         }
     }
 }
